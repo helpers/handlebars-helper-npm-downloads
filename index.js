b0afcba540896694beeb78aaae1ad8bf8983a6ac
Brian Woodward <brian.woodward@gmail.com>
2016-01-22 12:48:52 -0500

    implementation

--- index.js
+++ index.js
@@ -7,6 +7,49 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+var utils = require('./utils');
+
+/**
+ * Get the npm download counts for the specified repository.
+ *
+ * ```handlebars
+ * {{! when registered with `engine-handlebars` as `downloads` }}
+ * {{downloads "micromatch"}}
+ * {{! 17985264 }}
+ * ```
+ *
+ * @param  {String} `repo` Name of the repository to calculate download counts for.
+ * @param  {Number|Object} `locals` Additional options to control how many days back to get. May be a number specifying last N days.
+ * @param  {Number} `locals.last` Specify last N days to get downloads for. (Defaults to all downloads).
+ * @param {Object} `options` Handlebars options.
+ * @param {Number} `options.hash.last` Specify last N days on to the handlebars hash. `{{downloads "micromatch" last=30}}`
+ * @param  {Function} `cb` Callback function with signature `(err, total)`.
+ * @api public
+ */
+
+module.exports = function downloads(repo, locals, options, cb) {
+  if (typeof repo !== 'string') {
+    var err = new TypeError('expected `repo` to be a string');
+    if (typeof repo === 'function') {
+      return repo(err);
+    }
+    throw err;
+  }
+
+  if (typeof options === 'function') {
+    cb = options;
+    options = locals;
+    locals = {};
+  }
+
+  if (typeof cb !== 'function') {
+    throw new TypeError('expected `cb` to be a function');
+  }
+  locals = locals || {};
+  if (utils.isNumber(locals)) {
+    locals = { last: locals };
+  }
+
+  var opts = utils.extend({}, locals, options.hash);
+  utils.helper(repo, opts, cb);
 };
