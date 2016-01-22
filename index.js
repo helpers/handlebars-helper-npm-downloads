5e9ddc3c9d3a2b11e77a8d52811c4b9846e83734
Brian Woodward <brian.woodward@gmail.com>
2016-01-22 12:15:58 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * handlebars-helper-npm-downloads <https://github.com/helpers/handlebars-helper-npm-downloads>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
